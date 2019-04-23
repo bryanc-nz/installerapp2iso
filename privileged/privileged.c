5087cc93bb5df0a156112f8df2d9bb23fb363ba6
Bryan Christianson <bryan@whatroute.net>
2019-04-24 09:07:16 +1200

    Add license info to source

--- privileged/privileged.c
+++ privileged/privileged.c
@@ -4,6 +4,20 @@
 //  Created by Bryan Christianson on 19/03/17.
 //  Copyright © 2017 Rodney Truck Parts Ltd. All rights reserved.
 //
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
 
 #include <dlfcn.h>
 #include <stdio.h>
